39783ad757d615a03d600aa6c25a9ad052b29118
mczernyk <mczernyk@gmail.com>
2022-07-27 00:32:07 -0400

    added previews to prod images

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
 const Product = ({ product: { image, name, slug, price, details } }) => {
+  const [index, setIndex] = useState(0);
+
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           {image && (<img
-            src={urlFor(image && image[0])}
+            src={urlFor(image && image[index])}
             // width={250}
             height={250}
             className="product-image"
           />)}
+          <div className="small-images-container">
+            {image && (image.slice(0,3)?.map((item, i) => (
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
+            )))}
+          </div>
+
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
         </div>
