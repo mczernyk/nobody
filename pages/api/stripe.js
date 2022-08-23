aa4b329e92018d4205980c4f76d1e1e81e634e7f
mczernyk <mczernyk@gmail.com>
2022-08-23 02:48:49 -0400

    apple pay - no www

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 stripe.applePayDomains.create({
-  domain_name: 'www.nobody.clothing'
+  domain_name: 'nobody.clothing'
 })
 
 export default async function handler(req, res) {
