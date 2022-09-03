7a51e638369522c715cb14c468106b8f234f1b0e
mczernyk <mczernyk@gmail.com>
2022-09-03 02:39:54 -0400

    added self tee

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -233,7 +233,7 @@ const ProductDetails = ({ product, products }) => {
 
         }
       }
-      if (name === "love tee" || name === "passenger tee" || name === "trust tee") {
+      if (name === "love tee" || name === "passenger tee" || name === "trust tee" || 'self tee') {
         if (event.target.value === 'black') {
           setIndex(0)
           setPreview(0)
