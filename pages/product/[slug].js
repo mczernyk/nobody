1843cc87620e917a4b93956f14f500b28d67edc1
mczernyk <mczernyk@gmail.com>
2024-04-22 20:00:57 -0400

    shirt order bug

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -385,7 +385,7 @@ const ProductDetails = ({ product, products }) => {
       }
 
 
-      if (name === "love tee" || name === "passenger tee" || name === "trust tee" || 'self tee' || 'planet tee' || 'vacation tee') {
+      if (name === "love tee" || name === "passenger tee" || name === "trust tee" ||  name === 'self tee' ||  name === 'planet tee' ||  name === 'vacation tee') {
         if (event.target.value === 'black') {
           setIndex(0)
           setPreview(0)
